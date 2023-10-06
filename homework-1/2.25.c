ba5f548c29eb6567fe969fdfcb14819f0fe96851
clayshaw <howardchen1222@gmail.com>
2023-10-06 19:23:59 +0800

    HomeWork-1

---
+++ homework-1/2.25.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+// 這個要用自己的名字寫
+int main(){
+    printf("    CCC  \n");
+    printf("  C     C \n");
+    printf(" C       C\n");
+    printf("  C     C\n\n");
+    printf("HHHHHHHHH\n");
+    printf("    H\n");
+    printf("    H\n");
+    printf("HHHHHHHHH\n");
+}
