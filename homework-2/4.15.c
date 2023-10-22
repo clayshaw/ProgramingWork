5c3fe7f46aa4c7248d523145b90a6a214c7c93a3
clayshaw <howardchen1222@gmail.com>
2023-10-22 16:10:38 +0800

    modify 4.15.c

--- homework-2/4.15.c
+++ homework-2/4.15.c
@@ -1,13 +1,10 @@
 #include<stdio.h>
+#include<math.h>
 
 
 int main(){
     const double compound[5]={0.1,0.105,0.11,0.115,0.12};
     for(int i=0;i<5;i++){
-        double mon=5000;
-        for(int j=0;j<15;j++){
-            mon = mon * (1+compound[i]);
-        }
-        printf("The money after 15 years is %lf  (%lf)\n",mon,compound[i]);
+        printf("The money after 15 years is %lf  (%lf)\n",5000*pow(1+compound[i],15),compound[i]);
     }
 }
