6e64da783f4602a0a51d8959b09054576a1e68d3
clayshaw <howardchen1222@gmail.com>
2023-10-26 01:48:57 +0800

    push

--- homework-2/4.15.c
+++ homework-2/4.15.c
@@ -3,8 +3,7 @@
 
 
 int main(){
-    const double compound[5]={0.1,0.105,0.11,0.115,0.12};
     for(int i=0;i<5;i++){
-        printf("The money after 15 years is %lf  (%lf)\n",5000*pow(1+compound[i],15),compound[i]);
+        printf("The money after 15 years is %lf  (%lf)\n",5000*pow(1.1+(0.005*i),15),1+(0.005*i));
     }
 }
