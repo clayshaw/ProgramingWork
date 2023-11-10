642e7b73b92523c5e3ab66197c5a5fe765bf7b9f
clayshaw <howardchen1222@gmail.com>
2023-11-10 18:12:18 +0800

    6.11.c modify

--- homework-4/6.11.c
+++ homework-4/6.11.c
@@ -1,8 +1,15 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
 
 int main(){
-    int arr[10]={616,5,6,4,63,2,6,8,1661,815};
-    for(int i=0;i<10;i++){
+    srand(time(0));
+    int arr[20]={0};
+    for(int i=0;i<20;i++){
+        arr[i] = rand() % INT_MAX;
+    }
+    for(int i=0;i<20;i++){
         for(int j=0;j<10-i-1;j++){
             if(arr[j] > arr[j+1]){
                 int tmp = arr[j];
