47fa1ae7e76abc540276dd597291914c4e184ade
clayshaw <howardchen1222@gmail.com>
2023-11-12 17:47:31 +0800

    6.11.c modify

--- homework-4/6.11.c
+++ homework-4/6.11.c
@@ -10,21 +10,22 @@ void swap(int *a,int *b){
 
 
 int main(){
-    srand(time(0));
-    int arr[20]={0};
+    srand(time(0)<<5);
+    int len = rand()%100+10;
+    int arr[len];
     printf("Original value is :\n");
-    for(int i=0;i<20;i++){
+    for(int i=0;i<len;i++){
         i[arr] = rand() % INT_MAX;
         printf("%d ",i[arr]);
     }
     putchar('\n');
     printf("New value is :\n");
-    for(int i=0;i<20;i++){
-        for(int j=0;j<20-i-1;j++){
+    for(int i=0;i<len;i++){
+        for(int j=0;j<len-i-1;j++){
             if(j[arr] > (j+1)[arr]){
                 swap(arr+j,arr+j+1);
             }
         }
     }
-    for(int i=0;i<20;i++) printf("%d ",i[arr]);
+    for(int i=0;i<len;i++) printf("%d ",i[arr]);
 }
