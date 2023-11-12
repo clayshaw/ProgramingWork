4e3f3c1016fba5c9e55cbc332c82d384ed11fa5d
clayshaw <howardchen1222@gmail.com>
2023-11-12 17:11:32 +0800

    6.11.c modify

--- homework-4/6.11.c
+++ homework-4/6.11.c
@@ -14,17 +14,17 @@ int main(){
     int arr[20]={0};
     printf("Original value is :\n");
     for(int i=0;i<20;i++){
-        arr[i] = rand() % INT_MAX;
-        printf("%d ",arr[i]);
+        i[arr] = rand() % INT_MAX;
+        printf("%d ",i[arr]);
     }
     putchar('\n');
     printf("New value is :\n");
     for(int i=0;i<20;i++){
         for(int j=0;j<20-i-1;j++){
-            if(arr[j] > arr[j+1]){
+            if(j[arr] > (j+1)[arr]){
                 swap(arr+j,arr+j+1);
             }
         }
     }
-    for(int i=0;i<20;i++) printf("%d ",arr[i]);
+    for(int i=0;i<20;i++) printf("%d ",i[arr]);
 }
