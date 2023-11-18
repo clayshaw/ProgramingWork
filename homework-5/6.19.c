6609133eee741364d0bacdbc4b97575765fa159b
clayshaw <howardchen1222@gmail.com>
2023-11-18 16:14:22 +0800

    6.19.c

--- homework-5/6.19.c
+++ homework-5/6.19.c
@@ -2,16 +2,17 @@
 #include<time.h>
 #include<stdlib.h>
 
+#define TIMES 36000
+
 int main(){
     srand(time(0));
     int count[13]={0};
-    for(int i=0;i<36000;i++){
+    for(int i=0;i<TIMES;i++){
         count[(rand()%6+1)+(rand()%6+1)]++;
     }
-    char s[2] =" ";
-    printf("2\t3\t4\t5\t6\t7\t8\t9\t10\t11\t12\n");
+    puts("2\t3\t4\t5\t6\t7\t8\t9\t10\t11\t12");
     for(int i=2;i<13;i++) printf("%d\t",count[i]);
-    printf("\n");
+    putchar('\n');
     for(int i=2;i<13;i++) printf("%.2f\t",(float)count[i]/36000);
-    printf("\n");
+    putchar('\n');
 }
