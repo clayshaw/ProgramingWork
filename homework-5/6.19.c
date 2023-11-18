1383dee0200efabc24cf993a6ec6597344e07584
clayshaw <howardchen1222@gmail.com>
2023-11-18 16:25:53 +0800

    6.19.c

--- homework-5/6.19.c
+++ homework-5/6.19.c
@@ -13,6 +13,6 @@ int main(){
     puts("2\t3\t4\t5\t6\t7\t8\t9\t10\t11\t12");
     for(int i=2;i<13;i++) printf("%d\t",count[i]);
     putchar('\n');
-    for(int i=2;i<13;i++) printf("%.2f\t",(float)count[i]/36000);
+    for(int i=2;i<13;i++) printf("%.3f\t",(float)count[i]/36000);
     putchar('\n');
 }
