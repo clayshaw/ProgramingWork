6ef6fcfb065c6ee6ee9decf9e0c7491bb5a8faea
clayshaw <howardchen1222@gmail.com>
2023-11-19 02:31:39 +0800

    6.19.c

--- homework-5/6.19.c
+++ homework-5/6.19.c
@@ -4,15 +4,18 @@
 
 #define TIMES 36000
 
+int add(int a,int b){
+    if(!a) return b;
+    return add((a&b)<<1,a^b);
+}
+
 int main(){
     srand(time(0));
     int count[13]={0};
     for(int i=0;i<TIMES;i++){
-        count[(rand()%6+1)+(rand()%6+1)]++;
+        count[add((add(rand()%6,1)),(add(rand()%6,1)))]++;
     }
-    puts("2\t3\t4\t5\t6\t7\t8\t9\t10\t11\t12");
-    for(int i=2;i<13;i++) printf("%d\t",count[i]);
-    putchar('\n');
-    for(int i=2;i<13;i++) printf("%.3f\t",(float)count[i]/36000);
+    printf("Sum\tTimes\tProbability\n");
+    for(int i=2;i<13;i++) printf("%d\t%d\t%.3f\n",i,count[i],(float)count[i]/36000);
     putchar('\n');
 }
